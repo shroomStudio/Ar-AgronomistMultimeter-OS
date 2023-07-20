df23fc538b65fc02fc62d610ada9e1c62c621a90
shroomStudio <alexandro.esparza.cortes@gmail.com>
2023-07-19 20:54:37 -0600

    S00003: Class for navigation created

---
+++ Ar-AgronomistMultimeter-OS/src/HardwareConfiguration/Ar_NavigationButtonsClass.cpp
@@ -0,0 +1,24 @@
+// ShroomCorp
+// Ar_NavigationButtonsClass.cpp
+// Description
+// Copyright*
+
+// Include files
+#include "Ar_NavigationButtonsClass.h"
+
+// Name spaces
+using namespace navigationButtons;
+
+
+Ar_NavigationButtonsClass::Ar_NavigationButtonsClass(/* args */)
+{
+  
+}
+
+Ar_NavigationButtonsClass::~Ar_NavigationButtonsClass()
+{
+
+}
+
+
+
---
+++ Ar-AgronomistMultimeter-OS/src/HardwareConfiguration/Ar_NavigationButtonsClass.h
@@ -0,0 +1,36 @@
+// ShroomCorp
+// Ar_NavigationButtonsClass.h
+// Description
+// Copyright*
+
+#ifndef AR_NAVIGATIONBUTTONSCLASS_H
+#define AR_NAVIGATIONBUTTONSCLASS_H
+
+#include "Ar_HardwareConfigurationMainClass.h"
+
+namespace navigationButtons
+{
+  
+
+}//End Namespace
+  
+  class Ar_NavigationButtonsClass
+  {
+  public:
+  Ar_NavigationButtonsClass(/* args */);
+  ~Ar_NavigationButtonsClass();
+
+  //Public methods
+  void initialConfigurationOfButtons(void);
+ 
+  private:
+  // Private static member declaration
+  
+  };
+
+
+
+#endif //End AR_NAVIGATIONBUTTONSCLASS_H
+
+
+
