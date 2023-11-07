a260d7ed2bef284ae90eb8b770a9f8f5a7d13c00
shroomStudio <alexandro.esparza.cortes@gmail.com>
2023-11-07 13:58:05 -0600

    S00004: New class structure

---
+++ Ar-AgronomistMultimeter-OS/src/sub_UserInterface/userInterfaceMainClass.cpp
@@ -0,0 +1,21 @@
+// ShroomCorp
+// main.cpp
+// Description
+// Copyright*
+
+//Headers 
+#include <Arduino.h>
+
+//Namespaces 
+using namespace std;
+
+void setup() 
+{
+
+}
+
+void loop()
+{
+
+
+}
