e7ea5449466dba13f376cdde8947a98026292bab
shroomStudio <alexandro.esparza.cortes@gmail.com>
2024-01-06 13:42:30 -0600

    S00005: User interface created to give struture to the program and avoid a lot of code in main class

---
+++ Ar-AgronomistMultimeter-OS/src/sub_UserInterface/userInterfaceClass.cpp
@@ -0,0 +1,10 @@
+#include "userInterfaceClass.h"
+
+
+userInterfaceClass::userInterfaceClass(){
+    // Constructor class usbConecttionClass
+}
+
+userInterfaceClass::~userInterfaceClass(){
+    //Destructor class usbConecttionClass
+}
---
+++ Ar-AgronomistMultimeter-OS/src/sub_UserInterface/userInterfaceClass.h
@@ -0,0 +1,22 @@
+#ifndef USERINTERFACECLASS_H
+#define USERINTERFACECLASS_H
+
+#include <Arduino.h>
+#include "commonDataTypes.h"
+
+class userInterfaceClass{
+    
+    public:
+    userInterfaceClass();
+    ~userInterfaceClass();
+
+    //Public Attributtes 
+    //Public Attributes
+    
+    private:
+    //Private Attributes
+    //Private Methods
+};
+
+#endif
+//end class USERINTERFACECLASS_H
