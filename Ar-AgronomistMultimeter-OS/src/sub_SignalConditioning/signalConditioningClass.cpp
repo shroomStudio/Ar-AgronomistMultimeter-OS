1b0752014373e07270ccd85c2f73ecc62b73947d
shroomStudio <alexandro.esparza.cortes@gmail.com>
2024-07-27 10:43:33 -0600

    S00016: Functionality to grapth the values of each sensor added to mappinLED method

--- Ar-AgronomistMultimeter-OS/src/sub_SignalConditioning/signalConditioningClass.cpp
+++ Ar-AgronomistMultimeter-OS/src/sub_SignalConditioning/signalConditioningClass.cpp
@@ -36,6 +36,8 @@ void signalConditioningClass::LedMapping(int ledIndex)
 {
     int sum = 0;
 
+    Serial.begin(9600);
+
     switch (ledIndex)
     {
     case NUM_RED_LED:
@@ -43,17 +45,29 @@ void signalConditioningClass::LedMapping(int ledIndex)
     for (int i = 0; i < NUMBER_OF_SAMPLES; i++)
     {
         sum += redLedVoltageMeasurement[i][NUM_RED_LED];
+
+        //Printing the sensor read to grapth it before to clear the array 
+        Serial.println(redLedVoltageMeasurement[i][NUM_RED_LED]);
+        delay(100);
+        //Cleaning the array to leave it ready beore the next sensing
+        redLedVoltageMeasurement[i][NUM_RED_LED] = {'\0'};
     }
 
     // Calculate the average
     redAverageRead = sum / NUMBER_OF_SAMPLES;
     sum = 0;
+     
     break;
     case NUM_YELLOW_LED:
     // Sum all the values in the array
     for (int i = 0; i < NUMBER_OF_SAMPLES; i++)
     {
         sum += yellowLedVoltageMeasurement[i][NUM_YELLOW_LED];
+        //Printing the sensor read to grapth it before to clear the array 
+        Serial.println(yellowLedVoltageMeasurement[i][NUM_YELLOW_LED]);
+        delay(100);
+        //Cleaning the array to leave it ready beore the next sensing
+        yellowLedVoltageMeasurement[i][NUM_YELLOW_LED] = {'\0'};
     }
 
     // Calculate the average
@@ -65,6 +79,11 @@ void signalConditioningClass::LedMapping(int ledIndex)
     for (int i = 0; i < NUMBER_OF_SAMPLES; i++)
     {
         sum += blueLedVoltageMeasurement[i][NUM_BLUE_LED];
+        //Printing the sensor read to grapth it before to clear the array 
+        Serial.println(blueLedVoltageMeasurement[i][NUM_BLUE_LED]);
+        delay(100);
+        //Cleaning the array to leave it ready beore the next sensing
+        blueLedVoltageMeasurement[i][NUM_BLUE_LED] = {'\0'};
     }
 
     // Calculate the average
@@ -77,6 +96,11 @@ void signalConditioningClass::LedMapping(int ledIndex)
     for (int i = 0; i < NUMBER_OF_SAMPLES; i++)
     {
         sum += infraredLedVoltageMeasurement[i][NUM_IR_LED];
+        //Printing the sensor read to grapth it before to clear the array 
+        Serial.println(infraredLedVoltageMeasurement[i][NUM_IR_LED]);
+        delay(100);
+        //Cleaning the array to leave it ready beore the next sensing
+        infraredLedVoltageMeasurement[i][NUM_IR_LED] = {'\0'};
     }
 
     // Calculate the average
