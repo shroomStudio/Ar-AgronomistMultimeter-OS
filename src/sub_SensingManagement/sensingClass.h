bc6a6dd409b4dab01af2ecf1fd2f878de91f51fd
shroomStudio <61298045+shroomStudio@users.noreply.github.com>
2025-12-06 16:06:35 -0600

    Focus on as7265 (#21)
    * Library for adafruit so changes that it become a custom library
    
    * New library custom working well need to be calibrate
    
    * Finally working as is expected the sensor AS7265

--- src/sub_SensingManagement/sensingClass.h
+++ src/sub_SensingManagement/sensingClass.h
@@ -23,7 +23,7 @@ Other colors: Purple LEDs typically have wavelengths between 370-410 nm, and ult
 #include <Arduino.h>
 #include "sub_UserInterface/commonDataTypes.h"
 #include "ExternalLibraries/Adafruit_AS7341.h"
-#include "ExternalLibraries/Adafruit_AS7265x.h"
+#include "ExternalLibraries/ShroomCorp_AS7265x.h"
 #include "sub_UserInterface/lcdDisplayClass.h"
 #include "sub_UserInterface/buttonsClass.h"
 #include "sub_SignalConditioning/signalConditioningClass.h"
@@ -87,7 +87,7 @@ class sensingClass{
     buttonsClass &buttonsSensing;
     signalConditioningClass &conditioningSensing;
     Adafruit_AS7341 as7341;
-    Adafruit_AS7265x as7265x;
+    ShroomCorp_AS7265x as7265x;
 };
 
 #endif
